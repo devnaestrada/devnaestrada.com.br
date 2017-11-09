a311c6e2d9f0ebc690197e7f8ec54e82a2ae2056
femontanha <femontanhanew@gmail.com>
2017-11-09 01:13:17 -0200

    Gulp will manage jekyll and files

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const sass = require('gulp-sass')
+const child = require('child_process')
+const gutil = require('gulp-util')
+const browserSync = require('browser-sync').create();
+
+const siteRoot = '_site'
+const cssFiles = '_scss/**/*.?(s)css'
+
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build', '--watch', '--incremental', '--drafts'])
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll:' + message))
+  }
+
+  jekyll.stdout.on('data', jekyllLogger)
+  jekyll.stderr.on('data', jekyllLogger)
+})
+
+gulp.task('css', () => {
+  gulp.src(cssFiles)
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('assets/css'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch(cssFiles, ['css'])
+})
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  })
+
+  gulp.watch(cssFiles, ['css'])
+})
+
+gulp.task('default', ['css', 'jekyll', 'serve'])
